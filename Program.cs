433fccf8b462148c86350cf815fd86d8bb85edc3
Manjularaj <manjularajanap@gmail.com>
2024-02-22 03:19:14 -0800

    updated

--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, Professor!");
 
-Console.WriteLine("Manjula Rajan- I Wanted to learn the deepth and logic of the subject so that I can use the knowledge wisely to build any kind of app effectively/effeciently");
+Console.WriteLine("My name is Manjula Rajan- I Wanted to learn the deepth and logic of the subject so that I can use the knowledge wisely to build any kind of app effectively/effeciently");
 
